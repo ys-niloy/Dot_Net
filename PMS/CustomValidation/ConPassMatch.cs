f34481458ecb1f112d32971ef1e87113d11ec3ea
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-07 00:05:19 -0800

    Initial commit

---
+++ PMS/CustomValidation/ConPassMatch.cs
@@ -0,0 +1,28 @@
+﻿using PMS.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMS.CustomValidation
+{
+    public class ConPassMatch: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var cus = validationContext.ObjectInstance as CustomerDTO;
+            if (cus.Password != null && value != null) {
+                if (cus.Password.Equals(value.ToString()))
+                {
+                    return ValidationResult.Success;
+                }
+                else {
+                    return new ValidationResult("Password and Confirm Password Mismatched");
+                }
+            }
+            return new ValidationResult("Check Values");
+            
+        }
+    }
+}
---
+++ PMS/DTOs/CustomerDTO.cs
@@ -0,0 +1,24 @@
+﻿using PMS.CustomValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMS.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [UniqueUName]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [ConPassMatch]
+        public string ConfPass { get; set; }
+    }
+}
