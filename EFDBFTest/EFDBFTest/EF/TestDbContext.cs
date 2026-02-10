f18df753d71a2d2df9178ba64c360639c891ca2f
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-10 13:12:29 +0600

    Adding EF Files

---
+++ EFDBFTest/EFDBFTest/EF/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using EFDBFTest.EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDBFTest.EF;
+
+public partial class TestDbContext : DbContext
+{
+    public TestDbContext()
+    {
+    }
+
+    public TestDbContext(DbContextOptions<TestDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DbConn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
