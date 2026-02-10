dda06f80c6ea8d393cda7624e2022de5a815174e
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-10 13:11:10 +0600

    Adding Controller

---
+++ EFDBFTest/EFDBFTest/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using EFDBFTest.EF;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFDBFTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        TestDbContext db;
+        public StudentController(TestDbContext db)
+        {
+            this.db = db;
+        }
+    }
+}
