029012e3cc34d5e78b9c1715ea98e625e6861671
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-11 12:59:10 +0600

    IntroMVC push

---
+++ IntroMVC/IntroMVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Cgpa { get; set; }
+        public string Address { get; set; }
+    }
+}
