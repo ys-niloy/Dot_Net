aef49bcb6505bfd50044cac2da13c41dd9dbfd85
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-14 23:31:41 +0600

    Controllers and DTOs

---
+++ EFCFWEBAPI/EFCFWEBAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using EFCFWEBAPI.DTOs;
+using EFCFWEBAPI.EF;
+using EFCFWEBAPI.EF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCFWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        UmSContext db;
+        public DepartmentController(UmSContext db)
+        {
+            this.db = db;
+        }
+        Mapper GetMapper() {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<DepartmentDTO,Department>().ReverseMap();
+            });
+            return new Mapper(config);
+        }
+
+        [HttpGet("all")]
+        public ActionResult All() {
+            var data = db.Departments.ToList();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public ActionResult Get(int id) {
+            var data = db.Departments.Find(id);
+            return Ok(data);
+        }
+        [HttpPost("create")]
+        public IActionResult Create(DepartmentDTO d) {
+            var dept = GetMapper().Map<Department>(d);
+            db.Departments.Add(dept);
+            db.SaveChanges();
+            return Ok(dept);
+        }
+    }
+}
---
+++ EFCFWEBAPI/EFCFWEBAPI/DTOs/DepartmentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCFWEBAPI.DTOs
+{
+    public class DepartmentDTO
+    {
+        public int DeptId { get; set; }
+        
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
