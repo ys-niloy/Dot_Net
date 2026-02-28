8cfeaab7e67b692fd357d5dba181253bb79facf2
Mohammad Yeasin Ali <ys.niloy.666@gmail.com>
2026-02-28 23:25:17 +0600

    Controllers folder

---
+++ DBCoreWEBAPI/DBCoreWEBAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using DBCoreWEBAPI.EF;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBCoreWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        readonly Fall25AContext db;
+        //resolving dependency injection
+        public DepartmentController(Fall25AContext db) { 
+            this.db = db;
+        }
+        [HttpGet("all")]
+        public IActionResult All() { 
+            var data = db.Departments.ToList();
+            return Ok(data);
+        }
+
+    }
+}
